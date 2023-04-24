eba9cca24e24437eb4b0635ca3577952e0773c31
harisha7 <harisha.srinivas74@gmail.com>
2023-04-24 16:41:28 +0200

    step definitions done

--- specs/step_definitions/remove-from-cart.js
+++ specs/step_definitions/remove-from-cart.js
@@ -11,35 +11,32 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 // if you want to reuse them in another step
 let searchedFor;
 
-
-
-Given('that I have searched for the {string}', (searchTerm) => {
+/*Given('that I have searched for the {string}', (searchTerm) => {
   searchedFor = searchTerm;
-
   cy.get('#search').type(searchTerm);
-});
+});*/
 
-When('I click the buy button {string} time', (clickCount) => {
-  for (let i = 1; i <= +clickCount; i++) {
+When('I click the removeOne button {string} time', (clickCount) => {
+  for (let i = 1; i <= +clickCount; i--) {
     cy.log("searchedFor", searchedFor);
     // find h2 that contains the product we have searched for
     cy.get('.product h2').contains(searchedFor)
       // find its product div
       .parents('.product')
-      // find the buy button inside the product div
-      .find('button').contains('Köp')
+      // find the - button inside the product div
+      .find('button').contains('-')
       // and click the button
       .click();
   }
 });
 
-Then('{string} {string} should be added to the cart', (quantity, productName) => {
+Then('{string} {string} should be removed from the cart', (quantity, productName) => {
+  cy.log(productName,quantity)
   // find a li (list item) in the cart that contains the product name
-  //cy.log("hej alla ni glada", productName, quantity);
   cy.get('#cart li')
     .contains(productName)
     // and also contains the correct quantity
-    .contains(quantity + 'st')
+    .contains(quantity - 'st')
     // we should 1 element
-    .should('have.length', 0);
+    .should('have.length', 1);
 });
