6bb9c34818668496a03cc079edbefa03ba6d61c6
harisha7 <harisha.srinivas74@gmail.com>
2023-04-28 11:06:42 +0200

    changes to the base url in the server with my domain www.harisha.nodehill.com to go live

--- baseUrl.js
+++ baseUrl.js
@@ -1,2 +1,4 @@
 // Change to the url of the project you are testing
 module.exports = 'http://localhost:3000'
+
+//changes to the base url in the server with my domain www.harisha.nodehill.com to go live
